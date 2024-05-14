ddedc68fdcf264a19d5fba47cbb32c8eae64a5d3
kimyong <right4570@naver.com>
2024-05-14 23:15:36 +0900

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
           <input
             id="todo-title"
             type="text"
-            maxLength={9}
+            maxLength={8}
             value={title}
             onChange={(event) => {
               setTitle(event.target.value);
